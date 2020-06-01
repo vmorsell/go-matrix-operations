df8efbc56b61367c3bcfac4706782d9da8a33850
Viktor <viktor@intrykk.se>
2020-06-01 18:43:18 +0200

    Improve test error reporting

--- matrix_test.go
+++ matrix_test.go
@@ -8,17 +8,17 @@ import (
 
 func equals(m1 Matrix, m2 Matrix) error {
 	if len(m1.values) != len(m2.values) {
-		return errors.New("Row dimension doesn't match")
+		return errors.New(fmt.Sprintf("Non-matching row dimensions. m1 has %d, m2 has %d.", len(m1.values), len(m2.values)))
 	}
 
 	if len(m1.values[0]) != len(m2.values[0]) {
-		return errors.New("Column dimension doesn't match")
+		return errors.New(fmt.Sprintf("Non-matching column dimensions. m1 has %d, m2 has %d.", len(m1.values[0]), len(m2.values[0])))
 	}
 
 	for i, rows := range m1.values {
 		for j, _ := range rows {
 			if m1.values[i][j] != m2.values[i][j] {
-				return errors.New(fmt.Sprintf("Values at position [%d][%d] doesn't match", i, j))
+				return errors.New(fmt.Sprintf("Values at position [%d][%d] doesn't match. m1 has %f, m2 has %f.", i, j, m1.values[i][j], m2.values[i][j]))
 			}
 		}
 	}
@@ -33,11 +33,11 @@ func TestDimensions(t *testing.T) {
 	}}
 
 	if m1.rows() != 2 {
-		t.Fatalf("Rows dimension method returned %d, expected %d", m1.rows(), 2)
+		t.Fatalf("Unexpected return. Rows dimension method returned %d, expected %d.", m1.rows(), 2)
 	}
 
 	if m1.cols() != 4 {
-		t.Fatalf("Cols dimension method returned %d, expected %d", m1.cols(), 4)
+		t.Fatalf("Unexpected return. Cols dimension method returned %d, expected %d.", m1.cols(), 4)
 	}
 }
 
@@ -57,7 +57,7 @@ func TestAddScalar(t *testing.T) {
 	error := equals(m1, correct)
 
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -76,7 +76,7 @@ func TestSubtractScalar(t *testing.T) {
 
 	error := equals(m1, correct)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -95,7 +95,7 @@ func TestMultiplyScalar(t *testing.T) {
 
 	error := equals(m1, correct)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -115,12 +115,12 @@ func TestAddMatrix(t *testing.T) {
 
 	m1, error := AddMatrix(m1, m2)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Failed. %s", error)
 	}
 
 	error = equals(m1, correct)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -140,12 +140,12 @@ func TestSubtractMatrix(t *testing.T) {
 
 	m1, error := SubtractMatrix(m1, m2)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Failed. %s", error)
 	}
 
 	error = equals(m1, correct)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -167,12 +167,12 @@ func TestMultiplyMatrix(t *testing.T) {
 
 	m1, error := MultiplyMatrix(m1, m2)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Failed. %s", error)
 	}
 
 	error = equals(m1, correct)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -191,7 +191,7 @@ func TestTranspose(t *testing.T) {
 
 	error := equals(m1, correct)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Unexpected return. %s", error)
 	}
 }
 
@@ -205,10 +205,10 @@ func TestTrace(t *testing.T) {
 
 	trace, error := Trace(m1)
 	if error != nil {
-		t.Fatalf("%s", error)
+		t.Fatalf("Failed. %s", error)
 	}
 
 	if trace != correct {
-		t.Fatalf("Trace result incorrect")
+		t.Fatalf("Unexpected return. Got %f, expected %f.", trace, correct)
 	}
 }
